2872a9955af8e8cb216764f687efdfae78c0e42d
rasheedmhd <starletgh@gmail.com>
2024-05-02 23:11:49 +0000

    baby steps 02

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,13 @@
 #[allow(unused_variables)]
-use std::env::{self, args};
-use std::io::stdin;
+use std::env;
+use std::fs;
+use std::io::{
+    stdin, BufReader, Result,
+};
+
 use std::process;
 
 fn main() {
-    // package com.craftinginterpreters.lox;
-    // import java.io.BufferedReader;
-    // import java.io.IOException;
-    // import java.io.InputStreamReader;
-    // import java.nio.charset.Charset;
-    // import java.nio.file.Files;
-    // import java.nio.file.Paths;
-    // import java.util.List;
-
-    // public class Lox {
-    // public static void main(String[] args) throws IOException {
-    // if (args.length > 1) {
-    //     System.out.println("Usage: jlox [script]");
-    //         System.exit(64);
-    //         } else if (args.length == 1) {
-    //             runFile(args[0]);
-    //         } else {
-    //             runPrompt();
-    //         }
-    //     }
-    // }
-
     // Baby Steps
     println!("Llama - A programming language impl from Robert Nystrom's Crafting Interpreters");
     get();
@@ -36,18 +18,45 @@ fn main() {
 pub fn get() {
     let args: Vec<String> = env::args().collect();
     if args.len() > 1 {
-        println!("Usage: llama {:#}", args[1]);
+        println!("Usage: llama {:#?}", args);
         process::exit(64);
     } else if args.len() == 1 {
-        run_file(&args[0]);
+        run_file(args[0].clone());
     } else {
         run_prompt();
     }
 }
 
-fn run_file(v: &String) {
-    // unimplemented!()
+// Llama is a scripting language, which means it executes directly from source. Our
+// interpreter supports two ways of running code. If you start jlox from the
+// command line and give it a path to a file, it reads the file and executes it
+
+// ﻿private static void runFile(String path) throws IOException {
+// byte[] bytes = Files.readAllBytes(Paths.get(path));
+// run(new String(bytes, Charset.defaultCharset()));
+// }
+fn run_file(path: String) {
+    let bytes = fs::read_to_string(path).expect("{path} doesn't exist");
+    run(String::from(bytes));
 }
-fn run_prompt() {
+fn run_prompt() -> Result<()> {
+    let mut input_stream_reader = String::new();
+    stdin().read_line(&mut input_stream_reader).expect("Enter Command");
+    // for (;;) {
+    //     System.out.print("> ");
+    //     String line = reader.readLine();
+    //     if (line == null) break;
+    //     run(line);
+    //     }
+    loop {
+        print!("> ");
+        if input_stream_reader.is_empty() {
+            break;
+        }
+        // run(input_stream_reader);
+    }
+    Ok(())
+}
+fn run(bytes: String) {
     // unimplemented!()
 }
