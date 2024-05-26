8dc69ba5b62bbfd5a3857daf41aa6668e95c3629
rasheedmhd <starletgh@gmail.com>
2024-05-26 23:19:10 +0000

    rust visitor pattern eg

--- src/expr.rs
+++ src/expr.rs
@@ -2,6 +2,16 @@
 
 use crate::token::Token;
 
+// To Do
+// Type refactor for literal value
+// pub enum LiteralValue {
+//     Integer(i32),
+//     Float(f64),
+//     String(String),
+//     // Add other variants as needed
+// }
+
+
 pub enum Expr {
     Binary(BinaryExpr),
     Grouping(GroupingExpr),
@@ -84,3 +94,60 @@ impl UnaryExpr {
         }
     }
 }
+
+// The data we will visit
+mod ast {
+    // pub enum Stmt {
+    //     Expr(Expr),
+    //     Let(Name, Expr),
+    // }
+    //
+    // pub struct Name {
+    //     value: String,
+    // }
+    //
+    // pub enum Expr {
+    //     IntLit(i64),
+    //     Add(Box<Expr>, Box<Expr>),
+    //     Sub(Box<Expr>, Box<Expr>),
+    // }
+
+}
+
+// The abstract visitor
+mod visit {
+    // use ast::*;
+    use crate::expr::{BinaryExpr, GroupingExpr, LiteralExpr, UnaryExpr};
+
+    // pub trait Visitor<T> {
+    //     fn visit_binary_expr(&mut self,   expr: &BinaryExpr)   -> T;
+    //     fn visit_grouping_expr(&mut self, expr: &GroupingExpr) -> T;
+    //     fn visit_literal_expr(&mut self,  expr: &LiteralExpr)  -> T;
+    //     fn visit_unary_expr(&mut self,    expr: &UnaryExpr)    -> T;
+    // }
+}
+
+// use ast::*;
+// use visit::*;
+
+// An example concrete implementation - walks the AST interpreting it as code.
+// struct Interpreter;
+// impl Visitor<i64> for Interpreter {
+//     fn visit_name(&mut self, n: &Name) -> i64 {
+//         panic!()
+//     }
+//     fn visit_stmt(&mut self, s: &Stmt) -> i64 {
+//         match *s {
+//             Stmt::Expr(ref e) => self.visit_expr(e),
+//             Stmt::Let(..) => unimplemented!(),
+//         }
+//     }
+//
+//     fn visit_expr(&mut self, e: &Expr) -> i64 {
+//         match *e {
+//             Expr::IntLit(n) => n,
+//             Expr::Add(ref lhs, ref rhs) => self.visit_expr(lhs) + self.visit_expr(rhs),
+//             Expr::Sub(ref lhs, ref rhs) => self.visit_expr(lhs) - self.visit_expr(rhs),
+//         }
+//     }
+// }
